41f388390fe6e2f80418c6c1b5838cf5111bd7c9
freemancho1 <freeman.cho@gmail.com>
2023-04-20 18:01:06 +0900

    20230420.1722

--- lib/main.dart
+++ lib/main.dart
@@ -2,14 +2,17 @@ import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:game_template/src/main_menu/main_menu_screen.dart';
+import 'package:go_router/go_router.dart';
 import 'package:game_template/config.dart';
+import 'package:game_template/src/crashlytics/crashlytics.dart';
 import 'package:logging/logging.dart';
 
 Logger _log = Logger('main.dart');
 
 Future<void> main() async {
   FirebaseCrashlytics? crashlytics;
-  // await guardWithCrashlytics(guardedMain, crashlytics: crashlytics);
+  await guardWithCrashlytics(guardedMain, crashlytics: crashlytics);
 }
 
 /// 로깅과 충돌 보고가 없으면 그냥 void main()임.
@@ -33,8 +36,28 @@ void guardedMain() {
   // Todo: 준비가되면 아래 각 컨트롤러를 완성해야 함.(광고, 게임서비스, 인앱결제)
 
   /// AdsController? adsController;
-  // GameServicesController? gameServicesController;
+  /// GameServicesController? gameServicesController;
   /// InAppPurchaseController? inAppPurchaseController;
 
   // Todo: runApp
+}
+
+class GameMain extends StatelessWidget {
+  // Todo: 계속 추가해 갈 것.
+  const GameMain({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+
+  static final _router = GoRouter(
+    routes: [
+      GoRoute(
+        path: '/',
+        builder: (context, state) => const MainMenuScreen(key: Key('main menu')),
+        routes: [],
+      )
+    ]
+  );
 }
---
+++ lib/src/main_menu/main_menu_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MainMenuScreen extends StatelessWidget {
+  const MainMenuScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Text("Main Menu Screen!");
+  }
+}
